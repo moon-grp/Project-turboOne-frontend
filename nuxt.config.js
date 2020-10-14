fd399fa2165b227e22195a1396bea4e002b93e74
emohex <olumidemm@gmail.com>
2020-10-14 18:16:57 +0100

    header and landing page

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - turbo',
-    title: 'turbo',
+    title: 'shawdy',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -51,7 +51,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      
       themes: {
         dark: {
           primary: colors.blue.darken2,
